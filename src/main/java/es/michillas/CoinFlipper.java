e952eb79019edd5b90e9abb60fce980f06ed32a0
Michillas <elrickimont@gmail.com>
2024-01-11 03:11:58 +0100

    CoinFlipper - First commit (Added local files)

---
+++ src/main/java/es/michillas/CoinFlipper.java
@@ -0,0 +1,220 @@
+package es.michillas;
+
+import java.io.*;
+import java.time.LocalTime;
+
+public  class CoinFlipper {
+    private static CoinFlipper instance; // patrón Singleton (yo hago patrones Singleton pa hacerme el chulo)
+
+    private boolean inGame = true;
+
+    private CoinFlipper() {
+        // Llama al método donde está main loop del juego
+        this.gameLoop();
+    }
+
+    public static CoinFlipper getInstance() {
+        if (instance == null) {
+            instance = new CoinFlipper();
+        }
+        return instance;
+    }
+
+    public void gameLoop() {
+        while (inGame) {
+            System.out.println("     ██ ██    ██ ███████ ████████     ███████ ██      ██ ██████      ██ ████████ \n" +
+                    "     ██ ██    ██ ██         ██        ██      ██      ██ ██   ██     ██    ██    \n" +
+                    "     ██ ██    ██ ███████    ██        █████   ██      ██ ██████      ██    ██    \n" +
+                    "██   ██ ██    ██      ██    ██        ██      ██      ██ ██          ██    ██    \n" +
+                    " █████   ██████  ███████    ██        ██      ███████ ██ ██          ██    ██    \n" +
+                    "                                                                                 \n" +
+                    "                                                                                 ");
+            System.out.println("                                     ██████  \n" +
+                    "                                    ██  ████ \n" +
+                    "                                    ██ ██ ██ \n" +
+                    "                                    ████  ██ \n" +
+                    "                                     ██████  \n" +
+                    "                                             \n" +
+                    "                                             ");
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+                System.out.println("Press enter to flip the coin");
+
+                // Para que el código no continue hasta que el usuario pulse enter.
+                int userInput = reader.read();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            for(int i = 0; i <= 2; i++) {
+                this.flipAnimation();
+            }
+
+            this.winnerWinnerChickenDinner(); // Como en el PUBG :0
+
+            inGame = false;
+        }
+    }
+
+    public void flipAnimation() {
+        int wait = 350;
+
+        System.out.println("                                     ██████  \n" +
+                "                                    ██  ████ \n" +
+                "                                    ██ ██ ██ \n" +
+                "                                    ████  ██ \n" +
+                "                                     ██████  \n" +
+                "         \n" +
+                "         ");
+        try {
+            Thread.sleep(wait);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("         \n" +
+                "         \n" +
+                "                                    ████████ \n" +
+                "         \n" +
+                "         \n" +
+                "         \n" +
+                "         \n");
+        try {
+            Thread.sleep(wait);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("                                     ██    \n" +
+                "                                      ██   \n" +
+                "                                       ██  \n" +
+                "                                        ██ \n" +
+                "                                         ██\n" +
+                "       \n" +
+                "   ");
+        try {
+            Thread.sleep(wait);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("                                       ██ \n" +
+                "                                       ██ \n" +
+                "                                       ██ \n" +
+                "                                       ██ \n" +
+                "                                       ██ \n" +
+                "   \n");
+        try {
+            Thread.sleep(wait);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("                                        ██ \n" +
+                "                                       ██  \n" +
+                "                                      ██   \n" +
+                "                                     ██    \n" +
+                "                                    ██     \n" +
+                "       \n");
+        try {
+            Thread.sleep(wait);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("         \n" +
+                "                                    ████████ \n" +
+                "         \n" +
+                "         \n" +
+                "         \n");
+        try {
+            Thread.sleep(wait);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("                                     ██████  \n" +
+                "                                    ██    ██ \n" +
+                "                                    ██    ██ \n" +
+                "                                    ██    ██ \n" +
+                "                                     ██████  \n" +
+                "         \n" +
+                "         ");
+        try {
+            Thread.sleep(wait);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("         \n" +
+                "         \n" +
+                "                                    ████████ \n" +
+                "         \n" +
+                "         \n" +
+                "         \n");
+        try {
+            Thread.sleep(wait);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("                                     ██    \n" +
+                "                                      ██   \n" +
+                "                                       ██  \n" +
+                "                                        ██ \n" +
+                "                                         ██\n" +
+                "       \n" +
+                "   ");
+        try {
+            Thread.sleep(wait);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("                                       ██ \n" +
+                "                                       ██ \n" +
+                "                                       ██ \n" +
+                "                                       ██ \n" +
+                "                                       ██ \n" +
+                "   \n");
+        try {
+            Thread.sleep(wait);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("                                        ██ \n" +
+                "                                       ██  \n" +
+                "                                      ██   \n" +
+                "                                     ██    \n" +
+                "                                    ██     \n" +
+                "       \n");
+        try {
+            Thread.sleep(wait);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("         \n" +
+                "                                    ████████ \n" +
+                "         \n" +
+                "         \n" +
+                "         \n");
+        try {
+            Thread.sleep(wait);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void winnerWinnerChickenDinner() {
+        int number = LocalTime.now().getSecond();
+        if(number%2 == 0) {
+            System.out.println("Heads! // Cara!");
+            System.out.println("                                     ██████  \n" +
+                    "                                    ██    ██ \n" +
+                    "                                    ██    ██ \n" +
+                    "                                    ██    ██ \n" +
+                    "                                     ██████  \n" +
+                    "         \n" +
+                    "         ");
+        } else {
+            System.out.println("Tails! // Cruz!");
+            System.out.println("                                     ██████  \n" +
+                    "                                    ██  ████ \n" +
+                    "                                    ██ ██ ██ \n" +
+                    "                                    ████  ██ \n" +
+                    "                                     ██████  \n" +
+                    "         \n" +
+                    "         ");
+        }
+    }
+}
---
+++ src/main/java/es/michillas/Main.java
@@ -0,0 +1,7 @@
+package es.michillas;
+
+public class Main {
+    public static void main(String[] args) {
+        CoinFlipper.getInstance();
+    }
+}
